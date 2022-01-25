aabc6244417fb47e023cc445b0dfdfbedf7aa6ac
Matthew Asaminew <mattasaminew@gmail.com>
2022-01-24 22:49:30 -0500

    Initial Commit

---
+++ watchOS NavLink SwiftUI Bug WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  watchOS NavLink SwiftUI Bug WatchKit Extension
+//
+//  Created by Matthew Asaminew on 2022-01-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ watchOS NavLink SwiftUI Bug WatchKit Extension/watchOS_NavLink_SwiftUI_BugApp.swift
@@ -0,0 +1,19 @@
+//
+//  watchOS_NavLink_SwiftUI_BugApp.swift
+//  watchOS NavLink SwiftUI Bug WatchKit Extension
+//
+//  Created by Matthew Asaminew on 2022-01-24.
+//
+
+import SwiftUI
+
+@main
+struct watchOS_NavLink_SwiftUI_BugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
